4ab88b898b80ccf6c047717e22cd90fbd63a86d3
Saksham-varshney1 <varshneysaksham5@gmail.com>
2024-09-05 00:45:11 +0530

    plant identifier

---
+++ app/components/Footer.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mt-16 text-center text-white">
+      <p>
+        &copy; 2024 Plant Identifier. Made with ❣️ by SAKSHAM VARSHNEY
+       
+        .
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
